fa125ee2242b33d8b071b9082d7f13b297cf913f
sanghoho <ash1151@ajou.ac.kr>
2019-11-28 21:34:20 +0900

    Update: Structure is changed as Package

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # fast-neural-style :city_sunrise: :rocket:
+
+> If you want to show original Pytorch `Fast-Neural-Style` Exmpale, this is the [Original Repo](https://github.com/pytorch/examples/tree/master/fast_neural_style)
+
+
+# Introduction
+
+기존의 `Pytorch` [example](https://github.com/pytorch/examples) 레포지터리의 [fast_neural_style](https://github.com/pytorch/examples/tree/master/fast_neural_style)는 [Perceptual Losses for Real-Time Style Transfer and Super-Resolution](https://arxiv.org/abs/1603.08155)를 구현해놓았습니다. 다만 CLI 환경에서 사용되고, `argparse`로 인자들을 넘겨주기 때문에, 인자의 수정이나 사용성 측면에서 불편함을 느꼈습니다. 
+
+그래서 코드를 Python의 **Package**로 리팩토링 하였으며, 인자를 받는 `HyperParameter` 클래스도 추가하였습니다.  
+
 This repository contains a pytorch implementation of an algorithm for artistic style transfer. The algorithm can be used to mix the content of an image with the style of another image. For example, here is a photograph of a door arch rendered in the style of a stained glass painting.
 
 The model uses the method described in [Perceptual Losses for Real-Time Style Transfer and Super-Resolution](https://arxiv.org/abs/1603.08155) along with [Instance Normalization](https://arxiv.org/pdf/1607.08022.pdf). The saved-models for examples shown in the README can be downloaded from [here](https://www.dropbox.com/s/lrvwfehqdcxoza8/saved_models.zip?dl=0).
@@ -9,32 +19,85 @@ The model uses the method described in [Perceptual Losses for Real-Time Style Tr
     <img src="images/output-images/amber-mosaic.jpg" height="440px">
 </p>
 
-## Requirements
-The program is written in Python, and uses [pytorch](http://pytorch.org/), [scipy](https://www.scipy.org). A GPU is not necessary, but can provide a significant speed up especially for training a new model. Regular sized images can be styled on a laptop or desktop using saved models.
 
 ## Usage
-Stylize image
-```
-python neural_style/neural_style.py eval --content-image </path/to/content/image> --model </path/to/saved/model> --output-image </path/to/output/image> --cuda 0
-```
-* `--content-image`: path to content image you want to stylize.
-* `--model`: saved model to be used for stylizing the image (eg: `mosaic.pth`)
-* `--output-image`: path for saving the output image.
-* `--content-scale`: factor for scaling down the content image if memory is an issue (eg: value of 2 will halve the height and width of content-image)
-* `--cuda`: set it to 1 for running on GPU, 0 for CPU.
 
-Train model
-```bash
-python neural_style/neural_style.py train --dataset </path/to/train-dataset> --style-image </path/to/style/image> --save-model-dir </path/to/save-model/folder> --epochs 2 --cuda 1
+### `HyperParameter` calss
+
+```python
+class HyperParameter:
+  def __init__(self, command, cuda, param_dict):
+    # Code
+
+  def set_train_parameter(self, param_dict):
+    # Code
+    
+    def set_eval_parameter(self, param_dict):
+      # Code
 ```
 
-There are several command line arguments, the important ones are listed below
-* `--dataset`: path to training dataset, the path should point to a folder containing another folder with all the training images. I used COCO 2014 Training images dataset [80K/13GB] [(download)](http://mscoco.org/dataset/#download).
+* `__init__`: 어떤 함수를 쓰는지, GPU를 사용하는지 그리고 인자들의 dictionary를 받습니다. 
+* `set_train_parameter`: train을 할 경우, ***param_dict***를 넘겨받습니다.
+* `set_eval_parameter`: eval을 할 경우, ***param_dict***를 넘겨받습니다.
+
+설정되지 않은 값 들은 default 값으로 설정됩니다. 
+
+> [코드](https://github.com/sanghoho/Fast-Neural-Style-Transfer-Pytorch/tree/master/neural_style/core/neural_style.py)의 `HyperParameter` class 참고
+
+### Stylize image
+```python
+from ParentDir.core import neural_style
+
+### For Evaluation
+param_dict = {
+    "content_image": "/path/to/content.jpg",
+    "output_image": "/path/to/output.jpg",
+    "model": "/path/to/model/checkpoint.pth or mod.model"
+}
+
+cuda = 1 # True
+
+param = neural_style.HyperParameter("eval", cuda, param_dict)
+neural_style.stylize(param)
+```
+
+
+학습 중 얻을 수 있는 **checkpoint**의 `.pth` 파일이나, 학습이 모두 끝난 뒤 얻을 수 있는 `.model` 파일을 지정해서 학습된 스타일로 **content image**를 변환시켜줍니다. 
+
+### Train model
+```python
+from ParentDir.core import neural_style
+
+### For Train
+param_dict = {
+    # "transfer_learning": 1,
+    # "checkpoint_model_dir": "/path/to/checkpoint/",
+
+    "dataset": "/path/to/COCO",
+    "style_image": "/path/to/style.jpg",
+    "save_model_dir": "/path/to/save/"
+
+}
+
+cuda = 1 # True
+
+param = neural_style.HyperParameter("train", cuda, param_dict)
+neural_style.stylize(param)
+```
+
+train을 하는데, 이전 학습에서 이어서 하고 싶은 경우 **transfer_learing** 인자를 1로 하고, checkpoint 폴더를 지정해두면 이어서 진행합니다.  
+
+
+I used COCO 2014 Training images dataset [80K/13GB] [(download)](http://mscoco.org/dataset/#download).
+
 * `--style-image`: path to style-image.
 * `--save-model-dir`: path to folder where trained model will be saved.
 * `--cuda`: set it to 1 for running on GPU, 0 for CPU.
 
 Refer to ``neural_style/neural_style.py`` for other command line arguments. For training new models you might have to tune the values of `--content-weight` and `--style-weight`. The mosaic style model shown above was trained with `--content-weight 1e5` and `--style-weight 1e10`. The remaining 3 models were also trained with similar order of weight parameters with slight variation in the `--style-weight` (`5e10` or `1e11`).
+
+## Requirements
+The program is written in Python, and uses [pytorch](http://pytorch.org/), [scipy](https://www.scipy.org). A GPU is not necessary, but can provide a significant speed up especially for training a new model. Regular sized images can be styled on a laptop or desktop using saved models.
 
 ## Models
 
---
+++ core/neural_style.py
@@ -0,0 +1,286 @@
+import argparse
+import os
+import sys
+import time
+import re
+
+import numpy as np
+import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+import torch.onnx
+
+from ..module.utils import Utils
+from ..module.transformer_net import TransformerNet
+from ..module.vgg import Vgg16
+
+
+class HyperParameter:
+    def __init__(self, command, cuda, param_dict):
+        self.cuda = cuda
+        self.command = command
+        
+        if self.cuda and not torch.cuda.is_available():
+            print("ERROR: cuda is not available, try running on CPU")
+            sys.exit(1)
+
+        if command == "train":
+            self.set_train_parameter(param_dict)
+        else:
+            self.set_eval_parameter(param_dict)
+    
+    def set_train_parameter(self, param_dict):
+        # Required
+        self.dataset = param_dict["dataset"]
+        self.style_image = param_dict["style_image"]
+        self.save_model_dir = param_dict["save_model_dir"]
+
+        # Option
+        self.checkpoint_model_dir = os.path.join(self.save_model_dir, "checkpoint")
+        self.transfer_learning = param_dict.get("transfer_learning", 0)
+        self.epochs = param_dict.get("epochs", 2)
+        self.batch_size = param_dict.get("batch_size", 4)
+        self.image_size = param_dict.get("image_size", 256)
+        self.style_size = param_dict.get("style_size") # None
+        self.seed = param_dict.get("seed", 42)
+        self.content_weight = param_dict.get("content_weight", 1e5)
+        self.style_weight = param_dict.get("style_weight", 1e10)
+        self.lr = param_dict.get("lr", 1e-3)
+        self.log_interval = param_dict.get("log_interval", 500)
+        self.checkpoint_interval = param_dict.get("checkpoint_interval", 2000)
+    
+    def set_eval_parameter(self, param_dict):
+        # Required
+        self.content_image = param_dict["content_image"]
+        self.output_image = param_dict["output_image"]
+        self.model = param_dict["model"]
+
+        # Option
+        self.content_scale = param_dict.get("content_scale") # None
+        self.export_onnx = param_dict.get("export_onnx")
+
+
+
+def check_paths(param):
+    try:
+        if not os.path.exists(param.save_model_dir):
+            os.makedirs(param.save_model_dir)
+        if param.checkpoint_model_dir is not None and not (os.path.exists(param.checkpoint_model_dir)):
+            os.makedirs(param.checkpoint_model_dir)
+    except OSError as e:
+        print(e)
+        sys.exit(1)
+
+
+def train(param):
+    device = torch.device("cuda" if param.cuda else "cpu")
+
+    np.random.seed(param.seed)
+    torch.manual_seed(param.seed)
+
+    transform = transforms.Compose([
+        transforms.Resize(param.image_size),
+        transforms.CenterCrop(param.image_size),
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.mul(255))
+    ])
+    train_dataset = datasets.ImageFolder(param.dataset, transform)
+    train_loader = DataLoader(train_dataset, batch_size=param.batch_size)
+
+    transformer = TransformerNet().to(device)
+    optimizer = Adam(transformer.parameters(), param.lr)
+    mse_loss = torch.nn.MSELoss()
+
+    vgg = Vgg16(requires_grad=False).to(device)
+    style_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.mul(255))
+    ])
+    style = Utils.load_image(param.style_image, size=param.style_size)
+    style = style_transform(style)
+    style = style.repeat(param.batch_size, 1, 1, 1).to(device)
+
+    features_style = vgg(Utils.normalize_batch(style))
+    gram_style = [Utils.gram_matrix(y) for y in features_style]
+
+    for e in range(param.epochs):
+        transformer.train()
+        agg_content_loss = 0.
+        agg_style_loss = 0.
+        count = 0
+        for batch_id, (x, _) in enumerate(train_loader):
+            n_batch = len(x)
+            count += n_batch
+            optimizer.zero_grad()
+
+            x = x.to(device)
+            y = transformer(x)
+
+            y = Utils.normalize_batch(y)
+            x = Utils.normalize_batch(x)
+
+            features_y = vgg(y)
+            features_x = vgg(x)
+
+            content_loss = param.content_weight * mse_loss(features_y.relu2_2, features_x.relu2_2)
+
+            style_loss = 0.
+            for ft_y, gm_s in zip(features_y, gram_style):
+                gm_y = Utils.gram_matrix(ft_y)
+                style_loss += mse_loss(gm_y, gm_s[:n_batch, :, :])
+            style_loss *= param.style_weight
+
+            total_loss = content_loss + style_loss
+            total_loss.backward()
+            optimizer.step()
+
+            agg_content_loss += content_loss.item()
+            agg_style_loss += style_loss.item()
+
+            if (batch_id + 1) % param.log_interval == 0:
+                mesg = "{}\tEpoch {}:\t[{}/{}]\tcontent: {:.6f}\tstyle: {:.6f}\ttotal: {:.6f}".format(
+                    time.ctime(), e + 1, count, len(train_dataset),
+                                  agg_content_loss / (batch_id + 1),
+                                  agg_style_loss / (batch_id + 1),
+                                  (agg_content_loss + agg_style_loss) / (batch_id + 1)
+                )
+                print(mesg)
+
+            if param.checkpoint_model_dir is not None and (batch_id + 1) % param.checkpoint_interval == 0:
+                transformer.eval().cpu()
+                ckpt_model_filename = "ckpt_epoch_" + str(e) + "_batch_id_" + str(batch_id + 1) + ".pth"
+                ckpt_model_path = os.path.join(param.checkpoint_model_dir, ckpt_model_filename)
+                torch.save(transformer.state_dict(), ckpt_model_path)
+                transformer.to(device).train()
+
+    # save model
+    transformer.eval().cpu()
+    save_model_filename = "epoch_" + str(param.epochs) + "_" + str(time.ctime()).replace(' ', '_') + "_" + str(
+        param.content_weight) + "_" + str(param.style_weight) + ".model"
+    save_model_path = os.path.join(param.save_model_dir, save_model_filename)
+    torch.save(transformer.state_dict(), save_model_path)
+
+    print("\nDone, trained model saved at", save_model_path)
+
+
+def stylize(param):
+    device = torch.device("cuda" if param.cuda else "cpu")
+
+    content_image = Utils.load_image(param.content_image, scale=param.content_scale)
+    content_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.mul(255))
+    ])
+    content_image = content_transform(content_image)
+    content_image = content_image.unsqueeze(0).to(device)
+
+    if param.model.endswith(".onnx"):
+        output = stylize_onnx_caffe2(content_image, param)
+    else:
+        with torch.no_grad():
+            style_model = TransformerNet()
+            state_dict = torch.load(param.model)
+            # remove saved deprecated running_* keys in InstanceNorm from the checkpoint
+            for k in list(state_dict.keys()):
+                if re.search(r'in\d+\.running_(mean|var)$', k):
+                    del state_dict[k]
+            style_model.load_state_dict(state_dict)
+            style_model.to(device)
+            if param.export_onnx:
+                assert param.export_onnx.endswith(".onnx"), "Export model file should end with .onnx"
+                output = torch.onnx._export(style_model, content_image, param.export_onnx).cpu()
+            else:
+                output = style_model(content_image).cpu()
+    Utils.save_image(param.output_image, output[0])
+
+
+def stylize_onnx_caffe2(content_image, param):
+    """
+    Read ONNX model and run it using Caffe2
+    """
+
+    assert not param.export_onnx
+
+    import onnx
+    import onnx_caffe2.backend
+
+    model = onnx.load(param.model)
+
+    prepared_backend = onnx_caffe2.backend.prepare(model, device='CUDA' if param.cuda else 'CPU')
+    inp = {model.graph.input[0].name: content_image.numpy()}
+    c2_out = prepared_backend.run(inp)[0]
+
+    return torch.from_numpy(c2_out)
+
+
+# def main():
+#     main_arg_parser = argparse.ArgumentParser(description="parser for fast-neural-style")
+#     subparsers = main_arg_parser.add_subparsers(title="subcommands", dest="subcommand")
+
+#     train_arg_parser = subparsers.add_parser("train", help="parser for training arguments")
+#     train_arg_parser.add_argument("--epochs", type=int, default=2,
+#                                   help="number of training epochs, default is 2")
+#     train_arg_parser.add_argument("--batch-size", type=int, default=4,
+#                                   help="batch size for training, default is 4")
+#     train_arg_parser.add_argument("--dataset", type=str, required=True,
+#                                   help="path to training dataset, the path should point to a folder "
+#                                        "containing another folder with all the training images")
+#     train_arg_parser.add_argument("--style-image", type=str, default="images/style-images/mosaic.jpg",
+#                                   help="path to style-image")
+#     train_arg_parser.add_argument("--save-model-dir", type=str, required=True,
+#                                   help="path to folder where trained model will be saved.")
+#     train_arg_parser.add_argument("--checkpoint-model-dir", type=str, default=None,
+#                                   help="path to folder where checkpoints of trained models will be saved")
+#     train_arg_parser.add_argument("--image-size", type=int, default=256,
+#                                   help="size of training images, default is 256 X 256")
+#     train_arg_parser.add_argument("--style-size", type=int, default=None,
+#                                   help="size of style-image, default is the original size of style image")
+#     train_arg_parser.add_argument("--cuda", type=int, required=True,
+#                                   help="set it to 1 for running on GPU, 0 for CPU")
+#     train_arg_parser.add_argument("--seed", type=int, default=42,
+#                                   help="random seed for training")
+#     train_arg_parser.add_argument("--content-weight", type=float, default=1e5,
+#                                   help="weight for content-loss, default is 1e5")
+#     train_arg_parser.add_argument("--style-weight", type=float, default=1e10,
+#                                   help="weight for style-loss, default is 1e10")
+#     train_arg_parser.add_argument("--lr", type=float, default=1e-3,
+#                                   help="learning rate, default is 1e-3")
+#     train_arg_parser.add_argument("--log-interval", type=int, default=500,
+#                                   help="number of images after which the training loss is logged, default is 500")
+#     train_arg_parser.add_argument("--checkpoint-interval", type=int, default=2000,
+#                                   help="number of batches after which a checkpoint of the trained model will be created")
+
+#     eval_arg_parser = subparsers.add_parser("eval", help="parser for evaluation/stylizing arguments")
+#     eval_arg_parser.add_argument("--content-image", type=str, required=True,
+#                                  help="path to content image you want to stylize")
+#     eval_arg_parser.add_argument("--content-scale", type=float, default=None,
+#                                  help="factor for scaling down the content image")
+#     eval_arg_parser.add_argument("--output-image", type=str, required=True,
+#                                  help="path for saving the output image")
+#     eval_arg_parser.add_argument("--model", type=str, required=True,
+#                                  help="saved model to be used for stylizing the image. If file ends in .pth - PyTorch path is used, if in .onnx - Caffe2 path")
+#     eval_arg_parser.add_argument("--cuda", type=int, required=True,
+#                                  help="set it to 1 for running on GPU, 0 for CPU")
+#     eval_arg_parser.add_argument("--export_onnx", type=str,
+#                                  help="export ONNX model to a given file")
+
+#     param = main_arg_parser.parse_param()
+
+#     if param.subcommand is None:
+#         print("ERROR: specify either train or eval")
+#         sys.exit(1)
+#     if param.cuda and not torch.cuda.is_available():
+#         print("ERROR: cuda is not available, try running on CPU")
+#         sys.exit(1)
+
+#     if param.subcommand == "train":
+#         check_paths(param)
+#         train(param)
+#     else:
+#         stylize(param)
+
+
+# if __name__ == "__main__":
+#     main()
